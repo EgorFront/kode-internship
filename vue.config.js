0f2c8a0bfaf7be2f32e22f443c7bed2f55e63c13
Lavrenov Egor <egor.uriewitch@gmail.com>
2020-10-27 14:21:58 +0200

    Иницализация конфигов - Конфигурация esLint - Конфигурация prettier - Конфигурация vsCode - Конфигурация stylelint

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,15 @@
 module.exports = {
-  transpileDependencies: ["vuetify"]
+  transpileDependencies: ["vuetify"],
+
+  chainWebpack(config) {
+    // Set up all the aliases we use in our app.
+    config.resolve.alias.clear().merge(require('./aliases.config').webpack)
+    config.resolve.alias.merge({
+      vue$: 'vue/dist/vue.esm.js',
+    })
+
+    // Don't allow importing .vue files without the extension, as
+    // it's necessary for some Vetur autocompletions.
+    config.resolve.extensions.delete('.vue')
+  },
 };
