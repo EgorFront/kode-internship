6c1b2d221dbfad2d2f547eb9434d4f57c50def2d
Lavrenov Egor <egor.uriewitch@gmail.com>
2020-10-28 22:23:56 +0200

    Добавлена верстка страницы Login и базовая логика работы с формой авторизации - Добавлена вспомогательная функция wait для имитации задержки ответа от сервера

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,5 +1,116 @@
+<script>
+import wait from '@/src/utils/wait'
+import { required, minLength } from 'vuelidate/lib/validators'
+import maxLength from 'vuelidate/lib/validators/maxLength'
+
+export default {
+  data() {
+    return {
+      loading: false,
+      userLogin: 'KODE',
+      password: '123456',
+      hidePassword: true,
+    }
+  },
+
+  validations: {
+    userLogin: {
+      required,
+      minLength: minLength(4),
+      maxLength: maxLength(25),
+    },
+
+    password: {
+      required,
+      minLength: minLength(4),
+      maxLength: maxLength(25),
+    },
+  },
+
+  computed: {
+    loginError() {
+      return this.$helperValidate('userLogin', {
+        minLength: {
+          text: 'Минимальная длина логина {} символа',
+          param: 'min',
+        },
+        maxLength: {
+          text: 'Максимальная длина логина {} символов',
+          param: 'max',
+        },
+      })
+    },
+    passwordError() {
+      return this.$helperValidate('password', {
+        minLength: {
+          text: 'Минимальная длина пароля {} символа',
+          param: 'min',
+        },
+        maxLength: {
+          text: 'Максимальная длина пароля {} символов',
+          param: 'max',
+        },
+      })
+    },
+  },
+
+  methods: {
+    async tryLogin() {
+      this.$v.$touch()
+      if (this.$v.$error || this.loading) return void 0
+
+      this.loading = true
+
+      try {
+        await wait(2000)
+      } catch (error) {
+      } finally {
+        this.loading = false
+      }
+    },
+  },
+}
+</script>
+
 <template>
-  <div>
-    login
-  </div>
+  <v-flex xs12 sm8 md4 lg4>
+    <v-card class="elevation-1 pa-3">
+      <v-card-text>
+        <div class="layout column align-center">
+          <img
+            :src="require('@src/assets/logo.png')"
+            width="180"
+            height="180"
+          />
+          <h1 class="flex my-4 primary--text">Авторизация</h1>
+        </div>
+        <v-form ref="loginForm" @submit.prevent="tryLogin">
+          <v-text-field
+            v-model="userLogin"
+            append-icon="mdi-account"
+            name="login"
+            label="Login"
+            type="text"
+            :error-messages="loginError"
+          />
+          <v-text-field
+            id="password"
+            v-model="password"
+            :type="hidePassword ? 'password' : 'text'"
+            :append-icon="hidePassword ? 'mdi-eye-off' : 'mdi-eye'"
+            name="password"
+            label="Password"
+            :error-messages="passwordError"
+            @click:append="hidePassword = !hidePassword"
+          />
+        </v-form>
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn block color="primary" :loading="loading" @click="tryLogin">
+          Войти
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-flex>
 </template>
--- src/views/layouts/Authentication.vue
+++ src/views/layouts/Authentication.vue
@@ -5,11 +5,26 @@ export default {
 </script>
 
 <template>
-  <v-content>
+  <v-main class="login-wrap">
     <v-container fluid fill-height>
       <v-layout align-center justify-center>
         <router-view></router-view>
       </v-layout>
     </v-container>
-  </v-content>
+  </v-main>
 </template>
+
+<style lang="scss" scoped>
+.login-wrap {
+  &::before {
+    position: absolute;
+    top: 0%;
+    left: 50%;
+    width: 100%;
+    height: 50%;
+    content: '';
+    background-color: rgba(0, 0, 0, 0.75);
+    transform: translate(-50%, 0%);
+  }
+}
+</style>
